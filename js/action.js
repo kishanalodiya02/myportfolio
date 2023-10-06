a2c9ba8ddc06f687c812cb89d6303630a99f360f
kishanalodiya02 <144224316+kishanalodiya02@users.noreply.github.com>
2023-10-07 04:49:07 +0530

    myportfolio

---
+++ js/action.js
@@ -0,0 +1,27 @@
+var show = document.querySelector(".second-ul");
+var menu =document.querySelector(".menu-bar");
+       
+          function db(){
+
+        
+        
+
+    //  if(menu.firstElementChild.classList.contains("fa-bars")){
+    //     menu.firstElementChild.classList.replace("fa-bars","fa-times")
+    //  }    else{
+    //     menu.firstElementChild.classList.replace("fa-times","fa-bars")
+    //  }
+
+    show.setAttribute("style","display:block;")
+       
+          }
+
+          function db2(){
+           
+            show.removeAttribute("style")
+
+          }
+          
+
+          
+    
